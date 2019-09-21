44cb392b27107f1cb1b39340304dad3435e7f899
dvaseekara <53438081+dvaseekara@users.noreply.github.com>
2019-09-21 00:25:19 -0400

    Update setup_jenkins.sh

--- bin/setup_jenkins.sh
+++ bin/setup_jenkins.sh
@@ -29,7 +29,10 @@ oc new-build -D $'FROM docker.io/openshift/jenkins-agent-maven-35-centos7:v3.11\
 # Create pipeline build config pointing to the ${REPO} with contextDir `openshift-tasks`
 echo "oc new-build pipeline build from github jenkins file"
 oc new-build --strategy=pipeline --code=https://github.com/dvaseekara/advanced-openshift.git --context-dir=openshift-tasks --env=GUID=28e7 --env=REPO=https://github.com/dvaseekara/advanced-openshift.git --env=CLUSTER=https://master.na311.openshift.opentlc.com --name=task-pipeline
-oc start-buile task-pipeline
+
+echo "oc start-build task-pipeline"
+oc start-build task-pipeline
+
 # Make sure that Jenkins is fully up and running before proceeding!
 while : ; do
   echo "Checking if Jenkins is Ready..."
