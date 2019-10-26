be9a75262dd5d68af54ba5d544f424023a120819
Daniel Vaseekaran <danielvaseekaran@localhost.localdomain>
2019-10-25 22:11:46 -0400

    Auto commit

--- bin/setup_projects.sh
+++ bin/setup_projects.sh
@@ -21,6 +21,9 @@ if [ "$FROM_JENKINS" = "true" ]; then
   oc policy add-role-to-user admin ${USER} -n ${GUID}-jenkins
   oc policy add-role-to-user admin ${USER} -n ${GUID}-tasks-dev
   oc policy add-role-to-user admin ${USER} -n ${GUID}-tasks-prod
+ 
+  echo "Add service account role"
+  oc policy add-role-to-user admin system:serviceaccount:gpte-jenkins:jenkins -n ${GUID}-jenkins
 
   oc annotate namespace ${GUID}-jenkins    openshift.io/requester=${USER} --overwrite
   oc annotate namespace ${GUID}-tasks-dev  openshift.io/requester=${USER} --overwrite
